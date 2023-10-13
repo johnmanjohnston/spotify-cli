80104c65c92bc9468886223b3756f4f93ad08a3c
johnmanjohnston <c.ethanjohn@gmail.com>
2023-10-13 17:22:52 +0400

    display for cover image

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from auth import auth
+import climage
+import requests
 
 sp = auth()
 
 currentSongData = sp.current_playback()["item"]["album"]
+
+print()
+print(currentSongData)
+
+coverImage = (sp.current_playback()["item"]["album"]["images"][1]["url"])
+
+def download_file(url):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open("cover.png", 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                if chunk: 
+                    f.write(chunk)
+                    
+download_file(coverImage)
+coverAsText = climage.convert('cover.png', is_unicode=True, width=20)
+print(coverAsText)
