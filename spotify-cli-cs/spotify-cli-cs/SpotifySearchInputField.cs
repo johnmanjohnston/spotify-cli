3d5ef33158c9a9fb45d0109449740b6335ccdd1b
johnmanjohnston <c.ethanjohn@gmail.com>
2023-12-20 19:31:26 +0400

    search query entered in input field in TUI will search on Spotify's webpage

--- spotify-cli-cs/spotify-cli-cs/SpotifySearchInputField.cs
+++ spotify-cli-cs/spotify-cli-cs/SpotifySearchInputField.cs
@@ -1,10 +1,30 @@
-﻿namespace spotify_cli_cs.Components
+﻿using OpenQA.Selenium;
+
+namespace spotify_cli_cs.Components
 {
     class SpotifySearchInputField : TextInputField
     {
         public override void OnEnter()
         {
             System.Diagnostics.Debug.WriteLine("on enter but on the child class or smth idk");
+
+            // open search menu
+            var driver = SpotifyCLI.driver;
+            
+            var searchBtn = driver?.FindElement(By.XPath("//a[@href='/search']"));
+            searchBtn?.Click();
+
+            Thread.Sleep(10);
+
+            var searchInput = driver?.FindElement(By.XPath("//input[@data-testid='search-input']"));
+            searchInput?.Click();
+
+            // clear any existing content of searchInput
+            searchInput?.SendKeys(Keys.Control + "a");
+            searchInput?.SendKeys(Keys.Delete);
+
+            // send the content into searchInput using SendKeys()
+            searchInput?.SendKeys(content);
         }
     }
 }
--- spotify-cli-cs/spotify-cli-cs/TextInputField.cs
+++ spotify-cli-cs/spotify-cli-cs/TextInputField.cs
@@ -6,7 +6,7 @@ namespace spotify_cli_cs.Components
     {
         public TextInputField(int x = 0, int y = 0) : base(x, y) { }
 
-        private string? content; // content of the input field of this instance
+        protected string? content; // content of the input field of this instance
         public override void HandleKeyInput(ConsoleKey key)
         {
             System.Diagnostics.Debug.WriteLine("incoming key: " + key.ToString());
